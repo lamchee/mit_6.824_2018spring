e7d9246428bf19a6bd685f8ff56d49150ec4d36a
LamChee <lamchee@LamCheedeMacBook-Pro.local>
2018-07-02 22:27:30 +0800

    mapreduce-part3

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -1,6 +1,9 @@
 package mapreduce
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 //
 // schedule() starts and waits for all tasks in the given phase (mapPhase
@@ -30,5 +33,43 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 	//
 	// Your code here (Part III, Part IV).
 	//
+
+	//i := 0
+	var wg sync.WaitGroup
+	for i := 0; i < ntasks; i++ {
+		fmt.Printf("Try to catch a worker, current task index : %d, channel length : %d\n", i, len(registerChan))
+		worker := <-registerChan
+		fmt.Printf("Dispatch work, taskNumber : %d, worker : [%s],channel length : %d\n", i, worker, len(registerChan))
+
+		var args DoTaskArgs
+		args.JobName = jobName
+		args.NumOtherPhase = n_other
+		args.Phase = phase
+		args.TaskNumber = i
+		args.File = mapFiles[i]
+
+		wg.Add(1)
+		go func(w string, tasks int, args DoTaskArgs) {
+			defer wg.Done()
+			fmt.Printf("Going to process job, worker : [%s], taskNumber : %d\n", worker, args.TaskNumber)
+			call(worker, "Worker.DoTask", args, nil)
+
+			fmt.Printf("Put worker back to ch,cur channel length : %d, taskNumber : %d\n", len(registerChan), args.TaskNumber)
+			go func(worker string) {
+				for {
+					w, ok := <-registerChan
+					if ok {
+						registerChan <- w
+					}
+				}
+			}(w)
+
+			fmt.Printf("Job done! worker : [%s], taskNumber : %d\n", worker, args.TaskNumber)
+		}(worker, ntasks, args)
+
+	}
+	fmt.Printf("Going to wait...\n")
+	wg.Wait()
+	close(registerChan)
 	fmt.Printf("Schedule: %v done\n", phase)
 }
